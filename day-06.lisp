e6da5b8b0a134e82ee5c1882542136e4bd62132b
Ax <dlitvak98@protonmail.com>
2022-12-08 00:35:18 +0100

    Day 6 finished

--- day-06.lisp
+++ day-06.lisp
@@ -3,7 +3,7 @@
 (in-package #:aoc-2022)
 
 (defsolution (input-file) 6 1
-  (with-opn-file (in input-file)
+  (with-open-file (in input-file)
     (let ((str (read-line in))
 	  (buffer '()))
       (loop for i from 0
@@ -12,8 +12,35 @@
 	    do (progn
 		 (push c buffer)
 		 (when (check-buffer buffer)
-		   (format "N: ~a~%")
+		   (format t "N: ~a~%" (1+ i))
 		   (return))
-		 (setf buffer (butlast buffer)))))))
 
-(defun check-buffer (buffer))
+		 (if (>= (length buffer) 4)
+		     (setf buffer (butlast buffer))))))))
+
+(defsolution (input-file) 6 2
+  (with-open-file (in input-file)
+    (let ((str (read-line in))
+	  (buffer '()))
+      (loop for i from 0
+	    for c across str
+	    
+	    do (progn
+		 (push c buffer)
+		 (when (check-buffer buffer 14)
+		   (format t "N: ~a~%" (1+ i))
+		   (return))
+
+		 (if (>= (length buffer) 14)
+		     (setf buffer (butlast buffer))))))))
+
+(defun check-buffer (buffer &optional (len 4))
+  (unless (< (length buffer) len)
+    (let ((tmp '())
+	  (ret t))
+      (dolist (c buffer)
+	(if (assoc c tmp)
+	    (setf ret nil)
+	    (push (cons c nil) tmp)))
+
+      ret)))
